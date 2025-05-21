d44d51ff64d5d43b9ca4ac481937b77442807b15
miladsorour <milad.sorour.so@gmail.com>
2025-05-21 15:02:54 +0330

    add swagger

--- src/main/java/org/milad/wallet/WalletApplication.java
+++ src/main/java/org/milad/wallet/WalletApplication.java
@@ -1,11 +1,15 @@
 package org.milad.wallet;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.milad.wallet.config.WalletAppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "wallet", version = "0.0.1"))
 public class WalletApplication {
 
 	public static void main(String[] args) {
--- src/main/java/org/milad/wallet/config/SecurityConfiguration.java
+++ src/main/java/org/milad/wallet/config/SecurityConfiguration.java
@@ -59,7 +59,7 @@ public class SecurityConfiguration {
                 )
                 .authorizeHttpRequests(authz ->
                         authz
-                                .requestMatchers("/swagger-ui/**").permitAll()
+                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                                 .requestMatchers("/api/**").authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
